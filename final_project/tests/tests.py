289ff2b39bd78b63b955dab18eb31534eb4c5dce
Heiko-E <hegerter@posteo.de>
2022-10-02 22:22:40 +0200

    add negative tests

--- final_project/tests/tests.py
+++ final_project/tests/tests.py
@@ -15,6 +15,16 @@ class TestSquare(unittest.TestCase):
         english_text = translator.french_to_english(french_text)
         self.assertEqual(english_text, 'Hello')
 
+    def test_english_to_french_false(self):
+        english_text = 'Hallo'
+        french_text = translator.english_to_french(english_text)
+        self.assertNotEqual(french_text, 'Bonjour')
+
+    def test_french_to_english_false(self):
+        french_text = 'Hallo'
+        english_text = translator.french_to_english(french_text)
+        self.assertNotEqual(english_text, 'Hello')
+
 
 if __name__ == '__main__':
     unittest.main()
