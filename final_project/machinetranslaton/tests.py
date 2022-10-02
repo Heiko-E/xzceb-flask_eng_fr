461d67eddcffb168152eedf1d01031bccd0b9107
Heiko-E <hegerter@posteo.de>
2022-10-02 20:41:29 +0200

    update vscode settings

--- final_project/machinetranslaton/tests.py
+++ final_project/machinetranslaton/tests.py
@@ -1,19 +1,19 @@
 import unittest
-
 from translator import english_to_french, french_to_english
 
 
 class TestSquare(unittest.TestCase):
 
     def test_english_to_french(self):
-        english_text='Hello'
-        french_text= english_to_french(english_text)
+        english_text = 'Hello'
+        french_text = english_to_french(english_text)
         self.assertEqual(french_text, 'Bonjour')
 
     def test_french_to_english(self):
-        french_text= 'Bonjour'
-        english_text=french_to_english(french_text)
+        french_text = 'Bonjour'
+        english_text = french_to_english(french_text)
         self.assertEqual(english_text, 'Hello')
+
 
 if __name__ == '__main__':
     unittest.main()
